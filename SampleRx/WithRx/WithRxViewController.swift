3e7d4c9f2f1859dd17d9ad39f3065ce79fab6a09
Rin-t <rin.t.4921@gmail.com>
2023-02-18 19:01:05 +0900

    Rxを使用したサンプルの作成

--- SampleRx/WithRx/WithRxViewController.swift
+++ SampleRx/WithRx/WithRxViewController.swift
@@ -9,12 +9,53 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class WithRxViewController: UIViewController {
+final class WithRxViewController: UIViewController {
+
+    @IBOutlet private weak var button: UIButton!
+    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+
+    private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        button.rx.tap
+            .subscribe( onNext: { [weak self] in
+                guard let strongSelf = self else { return }
+                strongSelf.label.text = "押されたぁぁぁ"
+            })
+            .disposed(by: disposeBag)
+
+        Observable
+            .combineLatest(textField1.rx.text.orEmpty,
+                           textField2.rx.text.orEmpty)
+            .subscribe(onNext: { [weak self] text1, text2 in
+                guard let strongSelf = self else { return }
+                strongSelf.button.isHidden = !strongSelf.isValid(text1: text1, text2: text2)
+            })
+            .disposed(by: disposeBag)
     }
 
-
+    private func isValid(text1: String, text2: String) -> Bool {
+        text1.count > 2 &&  text2.count > 2 && text1 == text2
+    }
 }
 
+
+
+
+//
+//button.rx.tap
+//    .map { "押されたぁぁぁ" }
+//    .bind(to: label.rx.text)
+//    .disposed(by: disposeBag)
+//
+//Observable
+//    .combineLatest(textField1.rx.text.orEmpty,
+//                   textField2.rx.text.orEmpty)
+//    .map { self.isValid(text1: $0, text2: $1) }
+//    .map { !$0 }
+//    .bind(to: button.rx.isHidden)
+//    .disposed(by: disposeBag)
